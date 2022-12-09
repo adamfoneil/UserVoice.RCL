6739fa3d37ff29e0042a082dc80f4de6179bb6ab
Adam O'Neil <adamosoftware@gmail.com>
2022-12-09 06:17:32 -0500

    v1.0.13 markup strings where needed

---
+++ UserVoice.RCL/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace UserVoice.RCL.Extensions
+{
+    internal static class StringExtensions
+    {
+        internal static MarkupString ToMarkup(this string? input) => new MarkupString(input ?? string.Empty);
+    }
+}
