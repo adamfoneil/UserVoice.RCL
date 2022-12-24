a128b8343a625772f18651a9d6b1bf6797d9849d
Adam O'Neil <adamosoftware@gmail.com>
2022-12-24 11:50:00 -0500

    progress on release notes component

---
+++ UserVoice.RCL/Service/Queries/MyReleaseNotes.cs
@@ -0,0 +1,33 @@
+﻿using Dapper.QX;
+using Dapper.QX.Attributes;
+using UserVoice.Database;
+
+namespace UserVoice.RCL.Service.Queries
+{
+    public class MyReleaseNotes : Query<Item>
+    {
+        public MyReleaseNotes() : base(
+            @"IF NOT EXISTS(SELECT 1 FROM [uservoice].[ReleaseNoteMarker] WHERE [UserName]=@userName)
+            BEGIN
+                INSERT INTO [uservoice].[ReleaseNoteMarker] ([UserName], [VisibleAfter]) VALUES (@userName, '1/1/90')
+            END;
+
+            SELECT {top}
+                [i].*
+            FROM 
+                [uservoice].[Item] [i]
+                INNER JOIN [uservoice].[ReleaseNoteMarker] [m] ON [i].[DateCreated] > [m].[VisibleAfter] AND [m].[UserName]=@userName
+            WHERE
+                [i].[Type]=4 AND
+                [i].[IsActive]=1
+            ORDER BY 
+                [i].[DateCreated] DESC")
+        {
+        }
+
+        [Top]
+        public int? Top { get; set; }
+
+        public string UserName { get; set; } = default!;
+    }
+}
